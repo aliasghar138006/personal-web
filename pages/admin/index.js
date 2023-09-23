d887df81813dd29f2d5191cbf90edb5f66b20041
aliasghar138006 <aliasgharshahneh1380@gmail.com>
2023-09-23 12:48:39 +0330

    secound debug

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,16 +2,17 @@ import { data } from 'autoprefixer';
 import LoginPage from 'components/templates/LoginPage';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
+import { VerifyToken } from 'utils/passwordOperations';
 
 function Index(props) {
     const router = useRouter();
-    useEffect(() => {
-        fetch('/api/verify').then(res => res.json()).then(data => {
-            if(data.status == 'success'){
-                router.push('/dashboard');
-            }
-        })
-    },[])
+    // useEffect(() => {
+    //     fetch('/api/verify').then(res => res.json()).then(data => {
+    //         if(data.status == 'success'){
+    //             router.push('/dashboard');
+    //         }
+    //     })
+    // },[])
     return (
         <div>
             <LoginPage />
@@ -20,3 +21,19 @@ function Index(props) {
 }
 
 export default Index;
+
+export async function getServerSideProps(context){
+    const {token} = context.req.cookies;
+    const verifyToken = VerifyToken(token , process.env.SECRET_KEY);
+    if(!verifyToken){
+        return {
+            props:{}
+        }
+    }
+    return {
+        props:{verifyToken},
+        redirect : {
+            destination: '/dashboard'
+        }
+    }
+}
